5aebb226f0b899e7ceabad5439230f0ebebde7b9
Duc Lam <dinhduclam2001@gmail.com>
2023-04-09 20:11:23 +0700

    css for login & register page

--- views/login.php
+++ views/login.php
@@ -1,20 +1,20 @@
-<form action="" method="POST" id="form-login">
+<form action="" method="POST" id="form-auth">
 	<div class="auth-title">
 		<h1>Login</h1>
 		<p>Welcome back. Login to start working.</p>
 	</div>
 	<div class="main-form">
-		<div class="login-row">
+		<div class="auth-row">
 			<label>Email</label>
 			<input type="email" name="email" value="<?php echo $email ?? '' ?>" class="form-input" placeholder="Your email">
 		</div>
 
-		<div class="login-row">
+		<div class="auth-row">
 			<label>Password</label>
 			<input type="password" name="password" class="form-input" placeholder="Your password">
 		</div>
 
-		<div class="login-row checkbox">
+		<div class="auth-row checkbox">
 			<input type="checkbox" checked>
 			Keep me logged in
 		</div>
